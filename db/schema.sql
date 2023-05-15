6ce29dd06ffec1bd81edf69feb2dd52547d64177
maymyatnoehtet <may.mn.htet@gmail.com>
2023-05-15 10:55:07 +1000

    creating databse, tables in the database

---
+++ db/schema.sql
@@ -0,0 +1,35 @@
+-- Delete if there's an existing database named company_db
+DROP DATABASE IF EXISTS company_db;
+
+-- Create a company_db database
+CREATE DATABASE company_db;
+
+-- Use a company_db database
+USE company_db;
+
+-- Create departments table in company_db database
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(100) NOT NULL
+);
+
+-- Create roles table in company_db database
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    job_title VARCHAR(100) NOT NULL,
+    department_id INT,
+    salary INT
+);
+
+-- Create employees table in company_db database
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    job_id INT NOT NULL,
+    department_id INT NOT NULL,
+    salary INT,
+    manager_id INT
+
+);
+
